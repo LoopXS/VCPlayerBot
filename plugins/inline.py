fabe208de8e62e3df14fa6dba90ffd68e8b56b23
LoopXS <91501744+LoopXS@users.noreply.github.com>
2022-04-02 22:20:39 +0430

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -31,8 +31,7 @@ from pyrogram import (
 
 buttons = [
     [
-        InlineKeyboardButton('⚡️Make Own Bot', url='https://github.com/subinps/VCPlayerBot'),
-        InlineKeyboardButton('🧩 Join Here', url='https://t.me/subin_works'),
+        InlineKeyboardButton('✗ ᴅᴇᴠ ✗', url='https://t.me/DarkPentester'),
     ]
     ]
 def get_cmd(dur):
@@ -46,8 +45,8 @@ async def search(client, query):
     if query.query == "ETHO_ORUTHAN_PM_VANNU":
         answers.append(
             InlineQueryResultArticle(
-                title="Deploy",
-                input_message_content=InputTextMessageContent(f"{Config.REPLY_MESSAGE}\n\n<b>You can't use this bot in your group, for that you have to make your own bot from the [SOURCE CODE](https://github.com/subinps/VCPlayerBot) below.</b>", disable_web_page_preview=True),
+                title="VCPlayer",
+                input_message_content=InputTextMessageContent(f"{Config.REPLY_MESSAGE}\n\n<b>You Can't Use This Bot In Your Group !", disable_web_page_preview=True),
                 reply_markup=InlineKeyboardMarkup(buttons)
                 )
             )
